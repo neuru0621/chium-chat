84ac5b87386b826ef13d5641b6455b717c024f9d
myleobot <myleobot@gmail.com>
2022-06-28 10:57:02 +0900

    test

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config({ path: __dirname + "/.env" });
-var app = require("express")();
-var server = require("http").createServer(app);
-const websocket = require("./modules/socket");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const router = require("./routes");
+require('dotenv').config({ path: __dirname + '/.env' })
+var app = require('express')()
+var server = require('http').createServer(app)
+const websocket = require('./modules/socket')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = require('./routes')
+// 테스트 주석 추가
+app.use(cors())
+app.use(bodyParser.json())
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use('/', router)
 
-app.use("/", router);
-
-websocket(server, app);
+websocket(server, app)
 server.listen(3001, function () {
-  console.log("socket io server listening on port 3001");
-});
+  console.log('socket io server listening on port 3001')
+})
