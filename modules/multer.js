9f67ee2b1628de275d1c83961ca8ce4e401029c4
myleobot <myleobot@gmail.com>
2022-06-28 11:29:38 +0900

    d

--- modules/multer.js
+++ modules/multer.js
@@ -1,9 +1,9 @@
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
-aws.config.loadFromPath(__dirname + "/../config/s3.json");
-
-const s3 = new aws.S3();
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
+aws.config.loadFromPath(__dirname + '/../config/s3.json')
+console.log('111')
+const s3 = new aws.S3()
 exports.uploadImg = multer({
   storage: multerS3({
     s3: s3,
@@ -11,12 +11,12 @@ exports.uploadImg = multer({
     acl: process.env.ACL,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
-      cb(null, Date.now() + "." + file.originalname.split(".").pop());
+      cb(null, Date.now() + '.' + file.originalname.split('.').pop())
     },
   }),
   // 아래는 5mb 까지만 허용함을 의미
   limits: { fileSize: 5 * 1024 * 1024 },
-});
+})
 
 exports.uploadVideo = multer({
   storage: multerS3({
@@ -25,9 +25,9 @@ exports.uploadVideo = multer({
     acl: process.env.ACL,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
-      cb(null, Date.now() + "." + file.originalname.split(".").pop());
+      cb(null, Date.now() + '.' + file.originalname.split('.').pop())
     },
   }),
   // 아래는 25mb 까지만 허용함을 의미
   limits: { fileSize: 25 * 1024 * 1024 },
-});
+})
